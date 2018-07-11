c78ebb7c5f92173e08a79ec0234d2a95c8699776
bouzuya <m@bouzuya.net>
2018-07-11 23:06:09 +0900

    add Fetch

---
+++ src/Fetch.js
@@ -0,0 +1,21 @@
+"use strict";
+
+exports.fetchImpl = function (options) {
+  return function () {
+    return require('axios')({
+      data: options.body,
+      headers: options.headers,
+      method: options.method,
+      responseType: 'text',
+      url: options.url
+    });
+  };
+};
+
+exports.textImpl = function (response) {
+  return response.data;
+};
+
+exports.statusImpl = function (response) {
+  return response.status;
+};
