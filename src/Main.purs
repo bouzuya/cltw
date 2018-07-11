bd2d9787904fe07d80b666154b72a1b575e93941
bouzuya <m@bouzuya.net>
2018-07-11 23:06:41 +0900

    add dummy request

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,23 @@
 module Main
   (main) where
 
+import Data.Options ((:=))
 import Effect (Effect)
-import Effect.Class.Console (log)
-import Prelude (Unit)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Class.Console (log, logShow)
+import Fetch (fetch)
+import Fetch.Options (defaults, method, url)
+import Prelude (Unit, bind, (<>))
 
 main :: Effect Unit
-main = log "Hello"
+main = do
+  _ <- log "Hello"
+  launchAff_ do
+    response <-
+      fetch
+        ( defaults
+        <> method := "GET"
+        <> url := "https://bouzuya.net"
+        )
+    liftEffect (logShow response.status)
