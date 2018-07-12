c8c16153c64d248ef1bde4a7097f2985418592df
bouzuya <m@bouzuya.net>
2018-07-12 23:53:56 +0900

    parse Array

--- src/Main.purs
+++ src/Main.purs
@@ -2,6 +2,7 @@ module Main
   (main) where
 
 import Data.Argonaut.Core (Json)
+import Data.Argonaut.Core as Json
 import Data.Argonaut.Parser (jsonParser)
 import Data.Either (either)
 import Data.Maybe (Maybe(..))
@@ -14,7 +15,7 @@ import Fetch (fetch)
 import Fetch.Options (defaults, method, url)
 import Prelude (Unit, bind, compose, const, join, map, pure, (<>))
 
-fetchRepos :: Aff (Maybe Json)
+fetchRepos :: Aff (Maybe (Array Json))
 fetchRepos = do
   response <-
     fetch
@@ -27,7 +28,9 @@ fetchRepos = do
     toJson = compose (either (const Nothing) Just) jsonParser
     json :: Maybe Json
     json = join (map toJson response.body)
-  pure json
+    repoJsons :: Maybe (Array Json)
+    repoJsons = join (map Json.toArray json)
+  pure repoJsons
 
 main :: Effect Unit
 main = launchAff_ do
