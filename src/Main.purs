6aab0924e4c7d733a43a4e73b71510fb9e0db63f
bouzuya <m@bouzuya.net>
2018-07-12 23:39:00 +0900

    extract fetchRepos

--- src/Main.purs
+++ src/Main.purs
@@ -1,23 +1,35 @@
 module Main
   (main) where
 
+import Data.Argonaut.Core (Json)
+import Data.Argonaut.Parser (jsonParser)
+import Data.Either (either)
+import Data.Maybe (Maybe(..))
 import Data.Options ((:=))
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff (Aff, launchAff_)
 import Effect.Class (liftEffect)
-import Effect.Class.Console (log, logShow)
+import Effect.Class.Console (log)
 import Fetch (fetch)
 import Fetch.Options (defaults, method, url)
-import Prelude (Unit, bind, (<>))
+import Prelude (Unit, bind, compose, const, join, map, pure, (<>))
 
-main :: Effect Unit
-main = launchAff_ do
+fetchRepos :: Aff (Maybe Json)
+fetchRepos = do
   response <-
     fetch
       ( defaults
       <> method := "GET"
       <> url := "https://api.github.com/users/bouzuya/repos?type=owner&sort=pushed&direction=desc&per_page=100"
       )
-  _ <- liftEffect (logShow response.status)
-  _ <- liftEffect (logShow response.body)
+  let
+    toJson :: String -> Maybe Json
+    toJson = compose (either (const Nothing) Just) jsonParser
+    json :: Maybe Json
+    json = join (map toJson response.body)
+  pure json
+
+main :: Effect Unit
+main = launchAff_ do
+  _ <- fetchRepos -- TODO
   liftEffect (log "OK")
