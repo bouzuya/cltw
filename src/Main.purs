10d0b0ff1c4b9d09df58dd3eab8aa85137f114e0
bouzuya <m@bouzuya.net>
2018-07-14 10:46:19 +0900

    extract parseRepos

--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,7 @@ import Data.Argonaut.Core (Json)
 import Data.Argonaut.Core as Json
 import Data.Argonaut.Parser (jsonParser)
 import Data.Array as Array
-import Data.Either (either)
+import Data.Either (Either, either)
 import Data.Maybe (Maybe(..), maybe)
 import Data.Options ((:=))
 import Data.Traversable (traverse)
@@ -21,33 +21,34 @@ import Foreign.Object (Object)
 import Foreign.Object as Object
 import Prelude (Unit, bind, compose, const, join, map, pure, (<>), (>))
 
-fetchRepos :: Aff (Maybe (Array { fullName :: String, pushedAt :: String }))
+fetchRepos :: Aff (Maybe String)
 fetchRepos = do
-  response <-
-    fetch
-      ( defaults
-      <> method := "GET"
-      <> url := "https://api.github.com/users/bouzuya/repos?type=owner&sort=pushed&direction=desc&per_page=100"
-      )
+  response <- fetch
+    ( defaults
+    <> method := "GET"
+    <> url := "https://api.github.com/users/bouzuya/repos?type=owner&sort=pushed&direction=desc&per_page=100"
+    )
+  pure response.body
+
+parseRepos :: String -> Maybe (Array { fullName :: String, pushedAt :: String })
+parseRepos responseBody =
   let
+    maybeFromEither :: forall a b. Either a b -> Maybe b
+    maybeFromEither = either (const Nothing) Just
     toJson :: String -> Maybe Json
-    toJson = compose (either (const Nothing) Just) jsonParser
-    json :: Maybe Json
-    json = join (map toJson response.body)
-    repoJsons :: Maybe (Array Json)
-    repoJsons = join (map Json.toArray json)
-    repoObjects :: Maybe (Array (Object Json))
-    repoObjects = join (map (traverse Json.toObject) repoJsons)
+    toJson = compose maybeFromEither jsonParser
+    toRecords :: Json -> Maybe (Array { fullName :: String, pushedAt :: String })
+    toRecords json = do
+      array <- Json.toArray json
+      objects <- traverse Json.toObject array
+      pure (Array.catMaybes (map toRecord objects))
     toRecord :: Object Json -> Maybe { fullName :: String, pushedAt :: String }
     toRecord o = do
       fullName <- bind (Object.lookup "full_name" o) Json.toString
       pushedAt <- bind (Object.lookup "pushed_at" o) Json.toString
       pure { fullName, pushedAt }
-    f :: Array (Object Json) -> Array { fullName :: String, pushedAt :: String }
-    f a = Array.catMaybes (map toRecord a)
-    repoRecords :: Maybe (Array { fullName :: String, pushedAt :: String })
-    repoRecords = map f repoObjects
-  pure repoRecords
+  in
+    bind (toJson responseBody) toRecords
 
 fetchCommits :: String -> Aff (Maybe (Array Json))
 fetchCommits fullName = do
@@ -68,7 +69,7 @@ fetchCommits fullName = do
 
 main :: Effect Unit
 main = launchAff_ do
-  reposMaybe <- fetchRepos
+  reposMaybe <- (map (compose join (map parseRepos)) fetchRepos)
   repos <- liftEffect (maybe (throw "no repos") pure reposMaybe)
   let date = "2018-06-01T00:00:00Z" -- TODO
   let filteredRepos = Array.filter (\{ pushedAt } -> pushedAt > date) repos
